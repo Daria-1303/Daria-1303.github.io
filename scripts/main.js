f54cb51406b908ab8448fdd9b73c434f59b88ea6
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-11-12 22:49:36 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
